069cae8392d1c2a1ba86e17e816abe37cb250417
Andrew Boren <andrew.boren@gmail.com>
2013-07-19 18:12:21 -0700

    Tab layout toolbox....

---
+++ Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+#LOCAL_JAVA_LIBRARIES := bouncycastle
+#LOCAL_STATIC_JAVA_LIBRARIES := guava
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v13 android-support-v4
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := EVToolbox
+LOCAL_CERTIFICATE := platform
+
+include $(BUILD_PACKAGE)
